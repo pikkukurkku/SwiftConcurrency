b8ddbd54371cc9f811ab0e269b29cf1acb0112e6
pikkukurkku <natalia.ogorek@gmail.com>
2025-05-13 16:31:25 +0200

    Initial Commit

---
+++ Concurrency_SwiftUI/Concurrency_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Concurrency_SwiftUIApp.swift
+//  Concurrency_SwiftUI
+//
+//  Created by Natalia Ogorek on 13.05.25.
+//
+
+import SwiftUI
+
+@main
+struct Concurrency_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Concurrency_SwiftUITests/Concurrency_SwiftUITests.swift
@@ -0,0 +1,17 @@
+//
+//  Concurrency_SwiftUITests.swift
+//  Concurrency_SwiftUITests
+//
+//  Created by Natalia Ogorek on 13.05.25.
+//
+
+import Testing
+@testable import Concurrency_SwiftUI
+
+struct Concurrency_SwiftUITests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
